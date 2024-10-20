c6b20263a631e873148f7afb80721256356bfd78
Sounak Roy <iam.sounakroy@gmail.com>
2024-10-21 05:19:14 +0530

    templates

---
+++ dsa/template_cp.py
@@ -0,0 +1,12 @@
+def solve():
+    pass
+
+def main():
+    tc = int(input())
+
+    for _ in tc:
+        solve()
+
+
+if __name__ == "__main__":
+    main()
---
+++ dsa/template_lc.py
@@ -0,0 +1,15 @@
+class Solution:
+    def solve(self):
+        pass
+
+
+def main():
+    
+    # take inputs here
+
+    res = Solution().solve()
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
